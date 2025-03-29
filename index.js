49d35b8746a130a524ee604a72cf515dd04cf4de
ale-montes <amontes@frro.utn.edu.ar>
2025-03-29 11:13:28 -0300

    prueba node

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+// Configuración del puerto
+const PORT = 3000;
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
